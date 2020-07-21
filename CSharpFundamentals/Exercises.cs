246ac163d155ac1ccebd14beaf4f9647d57d79bd
Lester Molina <lecartuchen@gmail.com>
2020-07-20 23:15:01 -0400

    Started working on WwT-Ex-3

--- CSharpFundamentals/Exercises.cs
+++ CSharpFundamentals/Exercises.cs
@@ -691,6 +691,17 @@ namespace CSharpFundamentals
             }*/
 
             #endregion
+
+            #region Working with Text - Exercise 3
+
+            // Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+            // A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok";
+            // otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+
+            Console.WriteLine("Please enter a time value in the 24-hour format");
+            var inputTimeValue = Console.ReadLine();
+
+            #endregion
         }
     }
 }
